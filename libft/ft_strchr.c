a00e7b30c053bae88b76388467fa8a97d23e5cdb
VM windows Mike <elmikitozer@ubuntu.com>
2023-09-11 23:17:37 +0200

    adding ft_strrchr and ft_strchr, still didnt test a thing but norminette is ok

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,29 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: myevou <myevou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 23:01:32 by myevou            #+#    #+#             */
-/*   Updated: 2023/09/11 23:14:20 by myevou           ###   ########.fr       */
+/*   Created: 2023/09/11 23:07:58 by myevou            #+#    #+#             */
+/*   Updated: 2023/09/11 23:14:27 by myevou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strrchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	int		i;
-	char	*ptr;
 
 	i = 0;
-	ptr = 0;
 	while (s[i])
 	{
 		if (s[i] == c)
-			ptr = (char *)(s + i);
+			return ((char *)(s + i));
 		i++;
 	}
 	if (s[i] == c)
-		ptr = (char *)(s + i);
-	return (ptr);
+		return ((char *)(s + i));
 }
