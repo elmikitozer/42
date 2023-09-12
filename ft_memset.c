21ef39bacb88f31f6c73f346082b178af0317130
Mikaya Yevou <mikayay@MacBook-Pro-de-Mikaya.local>
2023-09-12 13:18:05 +0200

    adding memeset

--- ft_memset.c
+++ ft_memset.c
@@ -1,39 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mikayay <mikayay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 19:09:58 by myevou            #+#    #+#             */
-/*   Updated: 2023/09/12 13:05:31 by mikayay          ###   ########.fr       */
+/*   Created: 2023/09/12 12:59:48 by mikayay           #+#    #+#             */
+/*   Updated: 2023/09/12 13:16:59 by mikayay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	is_found(const char *str, const char *to_find)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	int	i;
+	size_t	i;
 
+	if (!b)
+		return (0);
 	i = 0;
-	while (to_find[i])
+	while (i < len)
 	{
-		if (to_find[i] != str[i])
-			return (0);
+		*(unsigned char*)(b + i) = (unsigned char)c;
 		i++;
 	}
-	return (1);
-}
-
-char	*ft_strnstr(const char *s1, const char *s2, size_t len)
-{
-	if (!s2[0])
-		return (s1);
-	while (*s1 && len)
-	{
-		if (s1[0] == s2[0] && is_found(s1, s2) != 0)
-			return (s1);
-		s1++;
-		len--;
-	}
-	return (0);
+	return (b);
 }
